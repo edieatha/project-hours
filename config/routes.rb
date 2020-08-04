baf0279914ee33fc45831f66e1cbb7f4414041ac
edieatha <edieatha@gmail.com>
2020-08-04 19:49:41 +0300

    Create sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/login'
+  get 'sessions/welcome'
   resources :users, only: [:new, :create]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
