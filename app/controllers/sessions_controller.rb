52ef609cdaae2739f69f075f037b40408f5f1fcf
edieatha <edieatha@gmail.com>
2020-08-04 20:51:19 +0300

    Create user and new user-session with username

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,22 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  skip_before_action :authorized, only: %i[new create welcome]
+
+  def new; end
 
   def create
+    @user = User.find_by(username: params[:username])
+    if @user
+      sessions[:user_id] = @user.id
+      redirect_to '/welcome'
+    else
+      redirect_to '/login'
+    end
   end
 
-  def login
-  end
+  def login; end
 
-  def welcome
-  end
+  def welcome; end
+
+  def page_requires_login; end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/login'
-  get 'sessions/welcome'
+  root 'sessions#welcome'
+  # get 'sessions/new'
+  # get 'sessions/create'
+  # get 'sessions/login'
+  # get 'sessions/welcome'
   resources :users, only: [:new, :create]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'welcome', to: 'sessions#welcome'
-  
-  # get 'users/new'
-  # get 'users/create'
+  get 'authorized', to: 'sessions#page_requires_login'
+
+  get 'users/new'
+  get 'users/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
