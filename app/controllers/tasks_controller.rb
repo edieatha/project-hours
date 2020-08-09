f7e107aabe9e6df3137853ca254b18ffc8039edb
edieatha <edieatha@gmail.com>
2020-08-10 00:38:23 +0300

    Add gravatar profile image Additional styling

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -37,7 +37,7 @@ class TasksController < ApplicationController
 
   def update
     @task.projects = []
-    @task.projects = Project.find(params[:task][:project_ids]) if params[:task][:project_ids]
+    @task.projects =Project Project.find(params[:task][:project_ids]) if params[:task][:project_ids]
 
     if @task.update(task_params)
       if params[:task][:project_ids]
