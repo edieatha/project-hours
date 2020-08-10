118e26f08a8cccaf757dfc75f1678f1c042a33f9
edieatha <edieatha@gmail.com>
2020-08-10 22:03:06 +0300

    Add status column to task table

--- db/migrate/20200806173355_create_tasks.rb
+++ db/migrate/20200806173355_create_tasks.rb
@@ -3,6 +3,7 @@ class CreateTasks < ActiveRecord::Migration[6.0]
     create_table :tasks do |t|
       t.string :task_name
       t.integer :task_hours
+      t.string :task_status
       t.integer :user_id
 
       t.timestamps
