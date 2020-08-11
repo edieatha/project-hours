8c248d508be99481b490151fb9d90f541f33b3bb
edieatha <edieatha@gmail.com>
2020-08-11 21:10:12 +0300

    Add login button on signup page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,13 @@ module ApplicationHelper
       link_to 'Home', current_user, class: 'text-white'
     end
   end
+  
+  def nav_links
+    if logged_in?
+      link_to "Log out", logout_path, class: "navbar-text nav-link text-decoration-none nav-item",
+                        method: :delete
+    else
+      link_to "Sign up", signup_path, class: "mx-2 nav-item navbar-text nav-link text-decoration-none"
+    end
+  end
 end
