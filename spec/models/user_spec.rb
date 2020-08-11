25a9ba41e5eda5a713a6b2e1e19dee8c77fab70c
edieatha <edieatha@gmail.com>
2020-08-11 03:00:47 +0300

    Add tests for user and task models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 RSpec.describe User, type: :model do
-  context "Validate user" do
+  context 'Validate user' do
     let(:user) { User.new(username: 'username') }
     let(:user2) { User.new(username: 'username2') }
 
@@ -18,13 +18,12 @@ RSpec.describe User, type: :model do
       user2.save
       user.username = 'username2'
       expect(user).not_to  be_valid
-
-    end  
+    end
   end
 
-  context "Associations for User" do
+  context 'Associations for User' do
     let(:user) { User.new(username: 'username') }
     it { should have_many(:tasks) }
-    it { should have_many(:projects) }    
-  end    
+    it { should have_many(:projects) }
+  end
 end
