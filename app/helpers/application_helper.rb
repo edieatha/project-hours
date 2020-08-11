283009f860584a771be165c76e4f6ec8b791da3d
edieatha <edieatha@gmail.com>
2020-08-11 22:18:58 +0300

    Linter errors fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,13 +4,12 @@ module ApplicationHelper
       link_to 'Home', current_user, class: 'text-white'
     end
   end
-  
+
   def nav_links
     if logged_in?
-      link_to "Log out", logout_path, class: "navbar-text nav-link text-decoration-none nav-item",
-                        method: :delete
+      link_to 'Log out', logout_path, class: 'navbar-text nav-link text-decoration-none nav-item', method: :delete
     else
-      link_to "Sign up", signup_path, class: "mx-2 nav-item navbar-text nav-link text-decoration-none"
+      link_to 'Sign up', signup_path, class: 'mx-2 nav-item navbar-text nav-link text-decoration-none'
     end
   end
 end
