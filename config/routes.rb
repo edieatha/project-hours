02260f8a5f3c514d1922c8ec541307b517028db2
edieatha <edieatha@gmail.com>
2020-08-12 23:34:45 +0300

    Remove redundant actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   root 'sessions#new'
   get '/signup', to: 'users#new'
-  resources :users only: [:new, :show ]
+  resources :users
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  resources :projects only: [:new, :index, :update, :show, :destory]
-  resources :tasks only: [:new, :index, :update, :show, :destory]
+  resources :projects
+  resources :tasks
 
   get '/tasks_no_project', to: 'tasks#index_tasks_no_project'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
