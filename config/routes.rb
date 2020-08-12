5aa5435d908ba9b955c847c6ada6ea40784f96e7
edieatha <edieatha@gmail.com>
2020-08-12 23:29:10 +0300

    Specify user actions per controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   root 'sessions#new'
   get '/signup', to: 'users#new'
-  resources :users
+  resources :users only: [:new, :show ]
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  resources :projects
-  resources :tasks
+  resources :projects only: [:new, :index, :update, :show, :destory]
+  resources :tasks only: [:new, :index, :update, :show, :destory]
 
   get '/tasks_no_project', to: 'tasks#index_tasks_no_project'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
