2942e960574b70e4e04ca72ef4b109a5be01a663
edieatha <edieatha@gmail.com>
2020-08-13 01:25:46 +0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
 
   def logged_in_user
     return unless logged_in?
+    
     store_location
     flash[:danger] = 'Please log in.'
     redirect_to login_url
